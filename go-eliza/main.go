7ee756a8ca51b6761cc7a31fe654202dbd6347d9
Gokul <gokul@ajirasoft.com>
2018-03-14 14:25:25 +0530

    refactors patterns to enable capturing groups

--- go-eliza/main.go
+++ go-eliza/main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"regexp"
 	"fmt"
+	"strings"
 )
 
 type ElizaResponses struct {
@@ -23,15 +24,26 @@ func (r *Reflection) reflect(in string) string {
 
 func (e *ElizaResponses) respond(s string) string {
 	for pattern, replies := range e.patterns {
-		match, err := regexp.MatchString(pattern, s)
-		if err != nil {
-			log.Fatal("Error while matching", err.Error())
-		}
+		r := regexp.MustCompile(pattern)
+		match := r.MatchString(s)
 		if match {
-			return fmt.Sprintf(replies[rand.Int()%len(replies)])
+			return fmt.Sprintf(getElizaResponse(replies[rand.Int()%len(replies)], getMatchedMap(r, s)))
 		}
 	}
 	return "Sorry I dont know what you are saying."
+}
+func getElizaResponse(s string, matchedMap map[string]string) string {
+	return strings.Replace(s, "%s", matchedMap["context"], -1)
+}
+func getMatchedMap(r *regexp.Regexp, s string) map[string]string {
+	paramsMap := make(map[string]string)
+	match := r.FindStringSubmatch(s)
+	for i, name := range r.SubexpNames() {
+		if i > 0 && i <= len(match) {
+			paramsMap[name] = match[i]
+		}
+	}
+	return paramsMap
 }
 
 var reflections = &Reflection{make(map[string]string)}
@@ -51,75 +63,75 @@ func main() {
 func initializeEliza() ElizaResponses {
 	eliza := ElizaResponses{make(map[string][]string)}
 	eliza.patterns = map[string][]string{
-		`I need (.*)`: {`Why do you need %s?`,
+		`I need (?P<context>.*)`: {`Why do you need %s?`,
 			`Would it really help you to get %s?`,
 			`Are you sure you need %s?`,
 		},
 
-		`Why don\'?t you ([^\?]*)\??`: {`Do you really think I don't %s?`,
+		`Why don\'?t you (?P<context>[^\?]*)\??`: {`Do you really think I don't %s?`,
 			`Perhaps eventually I will %s.`,
 			`Do you really want me to %s?`,
 		},
 
-		`Why can\'?t I ([^\?]*)\??`: {`Do you think you should be able to %s?`,
+		`Why can\'?t I (?P<context>[^\?]*)\??`: {`Do you think you should be able to %s?`,
 			`If you could %s, what would you do?`,
 			`I don't know -- why can't you %s?`,
 			`Have you really tried?`,
 		},
 
-		`I can\'?t (.*)`: {`How do you know you can't %s?`,
+		`I can\'?t (?P<context>.*)`: {`How do you know you can't %s?`,
 			`Perhaps you could %s if you tried.`,
 			`What would it take for you to %s?`,
 		},
 
-		`I am (.*)`: {`Did you come to me because you are %s?`,
+		`I am (?P<context>.*)`: {`Did you come to me because you are %s?`,
 			`How long have you been %s?`,
 			`How do you feel about being %s?`,
 		},
 
-		`I\'?m (.*)`: {`How does being %s make you feel?`,
+		`I\'?m (?P<context>.*)`: {`How does being %s make you feel?`,
 			`Do you enjoy being %s?`,
 			`Why do you tell me you're %s?`,
 			`Why do you think you're %s?`,
 		},
 
-		`Are you ([^\?]*)\??`: {`Why does it matter whether I am %s?`,
+		`Are you (?P<context>[^\?]*)\??`: {`Why does it matter whether I am %s?`,
 			`Would you prefer it if I were not %s?`,
 			`Perhaps you believe I am %s.`,
 			`I may be %s -- what do you think?`,
 		},
 
-		`What (.*)`: {`Why do you ask?`,
+		`What (?P<context>.*)`: {`Why do you ask?`,
 			`How would an answer to that help you?`,
 			`What do you think?`,
 		},
 
-		`How (.*)`: {`How do you suppose?`,
+		`How (?P<context>.*)`: {`How do you suppose?`,
 			`Perhaps you can answer your own question.`,
 			`What is it you're really asking?`,
 		},
 
-		`Because (.*)`: {`Is that the real reason?`,
+		`Because (?P<context>.*)`: {`Is that the real reason?`,
 			`What other reasons come to mind?`,
 			`Does that reason apply to anything else?`,
 			`If %s, what else must be true?`,
 		},
 
-		`(.*) sorry (.*)`: {`There are many times when no apology is needed.`,
+		`(?P<context>.*) sorry (?P<context>.*)`: {`There are many times when no apology is needed.`,
 			`What feelings do you have when you apologize?`,
 		},
 
-		`Hello(.*)`: {`Hello... I'm glad you could drop by today.`,
+		`Hello(?P<context>.*)`: {`Hello... I'm glad you could drop by today.`,
 			`Hi there... how are you today?`,
 			`Hello, how are you feeling today?`,
 		},
 
-		`I think (.*)`: {`Do you doubt %s?`,
+		`I think (?P<context>.*)`: {`Do you doubt %s?`,
 			`Do you really think so?`,
 			`But you're not sure %s?`,
 		},
 
-		`(.*) friend (.*)`: {`Tell me more about your friends.`,
+		`(?P<context>.*) friend (?P<context>.*)`: {`Tell me more about your friends.`,
 			`When you think of a friend, what comes to mind?`,
 			`Why don't you tell me about a childhood friend?`,
 		},
@@ -128,23 +140,23 @@ func initializeEliza() ElizaResponses {
 			`OK, but can you elaborate a bit?`,
 		},
 
-		`(.*) computer(.*)`: {`Are you really talking about me?`,
+		`(?P<context>.*) computer(?P<context>.*)`: {`Are you really talking about me?`,
 			`Does it seem strange to talk to a computer?`,
 			`How do computers make you feel?`,
 			`Do you feel threatened by computers?`,
 		},
 
-		`Is it (.*)`: {`Do you think it is %s?`,
+		`Is it (?P<context>.*)`: {`Do you think it is %s?`,
 			`Perhaps it's %s -- what do you think?`,
 			`If it were %s, what would you do?`,
 			`It could well be that %s.`,
 		},
 
-		`It is (.*)`: {`You seem very certain.`,
+		`It is (?P<context>.*)`: {`You seem very certain.`,
 			`If I told you that it probably isn't %s, what would you feel?`,
 		},
 
-		`Can you ([^\?]*)\??`: {`What makes you think I can't %s?`,
+		`Can you (?P<context>[^\?]*)\??`: {`What makes you think I can't %s?`,
 			`If I could %s, then what?`,
 			`Why do you ask if I can %s?`,
 		},
